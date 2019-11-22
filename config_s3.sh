ef00b83189811f660ebaa7bdb0a8276bf54dc6d3
Pavel Nakonechnyi <zorg@altlinux.org>
2019-11-22 17:59:08 +0100

    add config for the third season

---
+++ config_s3.sh
@@ -0,0 +1,10 @@
+
+BASEPATH="/mnt/nas/video/anime/Nodame Cantabile - Finale"
+
+EPISODE_FMT="Nodame Cantabile - Finale - %02d [DVDRip 1280x720 x264 FLAC].mkv"
+SUB_FMT="Nodame Cantabile - Finale - %02d \[DVDRip 1280x720 x264 FLAC\].ass"
+
+ASTREAM="0:a:0"
+ACODEC="flac -sample_fmt s16"
+
+SCALE="iw/2:ih/2"
